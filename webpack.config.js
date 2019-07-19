11be30c597ffe18bb17e9b49f7f6cc59c1ec7632
Julien SULPIS <julien.sulpis@gmail.com>
2019-07-19 23:05:52 +0200

    wip: add build config (bug remaining in a dependency)

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./index.ts",
+  target: "node",
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".json"]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([{ from: "static", to: "static" }, ".env"])
+  ]
+};
